6854bad517a68f2f98132802fc04d21817266583
Daniel Rech <daniel@nwebs.de>
2011-08-30 16:49:54 +0200

    bump version 0.4.2

--- ldtools.py
+++ ldtools.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.4"
+__version__ = "0.4.2"
 __useragent__ = ('ldtools-%s (http://github.com/dmr/ldtools, daniel@nwebs.de)'
                  % __version__)
 
