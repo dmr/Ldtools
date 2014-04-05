46679a818c472e7c0435ff2d61ee3f0b41850ce8
Daniel Rech <dr@nwebs.de>
2014-04-05 11:19:37 +0200

    Make example code look better

--- examples/dbpedia.py
+++ examples/dbpedia.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+
 import pprint
 
 import rdflib
@@ -11,47 +13,49 @@ from ldtools.helpers import set_colored_logger
 logger = set_colored_logger(2)
 
 
-def get_resource_and_connected_resources(uri, depth=1, **kw):
-    """Helper method to query uri and requery discovered Resouces.
-    kw will be passed to GET and GET_all"""
-
-    cnt = lambda: (len(Origin.objects.all()), len(Resource.objects.all()))
-
-    r,o = cnt()
-    logger.info("BEFORE %s: %s Origins and %s Resources" % (uri, r, o))
-
-    origin_uri = get_slash_url(uri)
-    origin, origin_created = Origin.objects.get_or_create(uri=origin_uri)
-    origin.GET(**kw)
-
-    r,o = cnt()
-    logger.info("AFTER lookup %s: %s Origins and %s Resources" % (origin_uri, r, o))
-
-    res, res_created = Resource.objects.get_or_create(uri=uri,
-        origin=origin)
-
-    if depth:
-        Origin.objects.GET_all(depth=depth, **kw)
-
-        r,o = cnt()
-        logger.info("AFTER GET_all: %s Origins and %s Resources" % (r, o))
-    return res
+count_things = lambda: (len(Origin.objects.all()), len(Resource.objects.all()))
 
 
-uri = "http://dbpedia.org/resource/Karlsruhe"
-karlsruhe = get_resource_and_connected_resources(
-    uri=uri,
-    depth=0,
+GET_kw = dict(
     only_follow_uris=[rdflib.RDFS.seeAlso, rdflib.OWL.sameAs],
     handle_owl_imports=True,
 )
 
 
-print "Print Karlsruhe Resource object"
-pprint.pprint(karlsruhe)
+from functools import wraps
 
-print
 
-print "Karlsruhe rdf:type s:"
-for type in list(karlsruhe.rdf_type):
-    print type
+def log_resource_n_origin_diff(func):
+    """
+    Logs hwo the storage's content changed during operation
+    """
+    @wraps(func)
+    def new_func(*args, **kwargs):
+        r1, o1 = count_things()
+        ret = func(*args, **kwargs)
+        r2, o2 = count_things()
+        logger.info(
+            "{0} more Origins and {1} more Resources".format(o2-o1, r2-r1)
+        )
+        return ret
+    return new_func
+
+
+@log_resource_n_origin_diff
+def get_resource_and_connected_resources(uri):
+    origin_uri = get_slash_url(uri)
+    origin, origin_created = Origin.objects.get_or_create(uri=origin_uri)
+    origin.GET(**GET_kw)
+    res, res_created = Resource.objects.get_or_create(uri=uri, origin=origin)
+    return res
+
+
+karlsruhe_uri = "http://dbpedia.org/resource/Karlsruhe"
+karlsruhe = get_resource_and_connected_resources(uri=karlsruhe_uri)
+print("Print Karlsruhe Resource object --> Number of properties:", len(karlsruhe.__dict__.keys()))
+
+print("Karlsruhe rdf:type s:")
+for typ in list(karlsruhe.rdf_type):
+    print(typ)
+
+log_resource_n_origin_diff(Origin.objects.GET_all(depth=1, **GET_kw))
